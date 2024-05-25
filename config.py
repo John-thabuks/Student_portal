3787a6f0b74ee739e96aaafcd3635a1f7b33eed4
John-thabuks <thabuksjohn@gmail.com>
2024-05-25 13:31:28 +0300

    Fix(deployment)

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
-from flask import Flask,request, make_response, jsonify
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import stripe
+
+import os
+
 
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] ="postgres://darasa_user:VxWloJVvTk5XoRxfeaSNtL24z3fmWb1m@dpg-cp8r1kf109ks739v9s9g-a/darasa"
+
+# app.config["SQLALCHEMY_DATABASE_URI"] ="postgres://darasa_user:VxWloJVvTk5XoRxfeaSNtL24z3fmWb1m@dpg-cp8r1kf109ks739v9s9g-a/darasa"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://darasa_user:VxWloJVvTk5XoRxfeaSNtL24z3fmWb1m@dpg-cp8r1kf109ks739v9s9g-a/darasa')
+
+
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = True
 app.config["SECRET_KEY"] = "92256b9d8a05214dab4362d83c9e17d1"
-stripe.api_key = 'sk_test_51PEpckRx876YYvellXk4uZw1hSPwac0nRQKYaCMb5QbYIpbgacTy6xEGf0x6A0JMXdEF17Igg111x9pL5wWFvd7300uTywvxxP'
+
 
 
 app.json.compact=False
@@ -19,4 +25,4 @@ db =SQLAlchemy()
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 db.init_app(app)
-app, origins="https://studentportal-yaoq.onrender.com"
+CORS(app, origins="https://studentportal-yaoq.onrender.com")
